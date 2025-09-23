29394ec65f02c91cda6a7b19032e6cca86152e04
AD_31 <dhamdherearyan20@gmail.com>
2025-09-23 19:23:38 +0530

    Privacy.jsx

--- Privacy.jsx
+++ Privacy.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+----------------------------------------
 export default function Privacy() {
   return (
     <div className="max-w-2xl mx-auto p-6 bg-white dark:bg-gray-800 rounded-lg shadow">
